4dcaa5ebed6fd4c15ce8a8141bbfdc7a0d92d9f5
nikolasmelui <slak@samaradom.ru>
2019-04-18 16:53:18 +0400

    Add new Mailer entity to sent email

---
+++ server/api/mailer/controllers/Mailer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Require the nodemailer module
+const nodemailer = require('nodemailer');
+
+module.exports = {
+  /**
+   * Send the email about new lead from the main form
+   */
+  leadmailer: async ctx => {
+    try {
+      // Define the reusable transport
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: `${process.env.EMAIL_ADDRESS}` || '',
+          pass: `${process.env.EMAIL_PASSWORD}` || ''
+        }
+      });
+      // Define the option object from the dotenv and request params
+      const options = {
+        from: `${process.env.EMAIL_ADDRESS}` || '',
+        to: `${process.env.EMAIL_ADDRESS}` || '',
+        replyTo: `no-reply@${process.env.EMAIL_ADDRESS}`,
+        subject: 'У вас новая заявка с сайта ...',
+        html: ctx.request.body.html || ''
+      };
+      //Send the email
+      await transporter.sendMail(options);
+      return {
+        statusCode: 200,
+        message: 'The email was successfully sent.'
+      };
+    } catch (err) {
+      ctx.body = err;
+    }
+  }
+};
