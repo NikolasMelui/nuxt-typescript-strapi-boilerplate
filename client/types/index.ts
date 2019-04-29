931ff515601654184b0c87e277bbb12c9a9f2d9d
nikolasmelui <slak@samaradom.ru>
2019-04-30 00:06:40 +0400

    Add new types in index.ts vuex types vile

--- client/types/index.ts
+++ client/types/index.ts
@@ -1,25 +1,99 @@
-export * from './state';
-
-export interface Person {
-  id: number;
-  first_name: string;
-  last_name: string;
-  contact: Contact;
-  gender: string;
-  ip_address: string;
-  avatar: string;
-  address: Address;
+// Store
+export interface RootState {
+  info: InfoState;
+  pages: PagesState;
+  posts: PostsState;
+  shared: SharedState;
 }
 
-export interface Contact {
-  email: string;
+export interface Image {
+  url: string;
+}
+
+export interface SharedState {
+  isMenuOpen: boolean;
+  isCallbackOpen: boolean;
+  isToolbarActive: boolean;
+  isAnimateMainScreen: boolean;
+  modalSettings: Object;
+  notification: Object;
+}
+
+export interface Info {
+  _id: string;
+  address_ru: string;
+  address_en: string;
+  addresslink: string;
   phone: string;
+  email: string;
+  vkontakte: string;
+  facebook: string;
+  instagram: string;
 }
 
-export interface Address {
-  city: string;
-  country: string;
-  postalCode: string;
-  state: string;
-  street: string;
+export interface InfoState {
+  info?: Info;
+}
+
+export interface Page {
+  _id: string;
+  slug: string;
+  seotitle: string;
+  seodescription: string;
+  seokeywords: string;
+  title_ru: string;
+  title_en: string;
+  subtitle_ru: string;
+  subtitle_en: string;
+  content_ru: string;
+  content_en: string;
+}
+
+export interface PagesState {
+  mainPage: Page;
+  aboutPage: Page;
+  postsPage: Page;
+  projectsPage: Page;
+}
+
+export interface Post {
+  _id: string;
+  createdAt: string;
+  slug: string;
+  title_ru: string;
+  title_en: string;
+  content_ru: string;
+  content_en: string;
+  previewImage: Image;
+  image: Image;
+}
+
+export interface PostsState {
+  posts?: Array<Post>;
+  postsTotalCount?: number;
+}
+
+export interface SeoState {
+  seotitle: string;
+  seodescription: string;
+  seokeywords: string;
+}
+
+export interface PostPage {
+  _id: string;
+  slug: string;
+  createdAt: string;
+  seotitle: string;
+  seodescription: string;
+  seokeywords: string;
+  title_ru: string;
+  title_en: string;
+  content_ru: string;
+  content_en: string;
+  previewImage: Image;
+  image: Image;
+}
+
+export interface PostPageState {
+  postPage: PostPage;
 }
