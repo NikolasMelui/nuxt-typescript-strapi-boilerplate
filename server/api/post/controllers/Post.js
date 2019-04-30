002c7b68868c8c4424840a53bf2dd52bbc394895
nikolasmelui <slak@samaradom.ru>
2019-04-30 18:34:53 +0400

    Add new routes and entities on the server strapi side

---
+++ server/api/post/controllers/Post.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Post.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Post`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve post records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx) => {
+    if (ctx.query._q) {
+      return strapi.services.post.search(ctx.query);
+    } else {
+      return strapi.services.post.fetchAll(ctx.query);
+    }
+  },
+
+  /**
+   * Retrieve a post record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    return strapi.services.post.fetch(ctx.params);
+  },
+
+  /**
+   * Count post records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx) => {
+    return strapi.services.post.count(ctx.query);
+  },
+
+  /**
+   * Create a/an post record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.post.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an post record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.post.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an post record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.post.remove(ctx.params);
+  }
+};
