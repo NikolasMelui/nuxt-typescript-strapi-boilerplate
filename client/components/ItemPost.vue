397a00a51e9e3dcca31dd1a11281c4f9d524e1ca
nikolasmelui <slak@samaradom.ru>
2019-04-30 18:36:33 +0400

    Add ItemPost component

---
+++ client/components/ItemPost.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  nuxt-link(:to="`/post/${post.slug}`")
+    div
+      h3 {{post.title_ru}}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+import { Post } from "~/types";
+
+@Component
+export default class ItemPost extends Vue {
+  @Prop(Object) post!: Post;
+}
+</script>
+
+<style lang="sass">
+</style>
