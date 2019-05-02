690a7b1cb02ab7855c2321bf512ae49b56a1fb58
nikolasmelui <slak@samaradom.ru>
2019-05-03 01:18:24 +0400

    Add tge error.vue layout

---
+++ client/layouts/error.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div
+    Toolbar
+    h3 {{ error }}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+import Toolbar from "~/components/Toolbar.vue";
+
+@Component({
+  components: {
+    Toolbar
+  }
+})
+export default class extends Vue {
+  @Prop(Object) error!: Error;
+
+  // async asyncData({ store, params, req, error, redirect }) {
+  // return redirect("/404");
+  // }
+}
+</script>
+
+<style lang="sass">
+</style>
