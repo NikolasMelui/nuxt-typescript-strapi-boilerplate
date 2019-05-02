9edbffe1326cc6ee72abf91634df4258104ed5e7
nikolasmelui <slak@samaradom.ru>
2019-05-02 20:56:21 +0400

    Add changes to the default.vue layout file

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,25 @@
-<template>
-  <div>
-    <nuxt/>
-  </div>
+<template lang="pug">
+  div
+    Toolbar
+    .main-container
+      nuxt
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+// Import components
+import Toolbar from "~/components/Toolbar.vue";
+
+@Component({
+  components: {
+    Toolbar
+  }
+})
+export default class extends Vue {}
+</script>
+
+<style lang="sass">
+.main-container
+  padding: 30px 10px
+</style>
