f416c033fa6cdde1c4e1478ee3e770c5262fb052
nikolasmelui <slak@samaradom.ru>
2019-05-02 20:24:33 +0400

    Add the Toolbar component

---
+++ client/components/Toolbar.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .toolbar
+    nuxt-link.link(to="/")
+      h3 Main page
+    nuxt-link.link(to="/posts/")
+      h3 Posts page
+</template>
+
+<script lang="ts">
+import { Vue } from "nuxt-property-decorator";
+
+export default class Toolbar extends Vue {}
+</script>
+
+<style lang="sass">
+.toolbar
+  display: flex
+  flex-direction: row
+  border-width: 2px 0
+  border-style: solid
+  border-color: wheat
+  .link
+    max-height: 39px
+    padding: 10px
+    h3
+      color: wheat
+      font-size: 18px
+      line-height: 20px
+      transition: .3s
+      &:hover
+        color: white
+        font-size: 21px
+</style>
