c1c0a100b0aeb317ff020d8e77a9853d8fa46a78
nikolasmelui <slak@samaradom.ru>
2019-05-07 21:15:39 +0400

    Add 404 layout

---
+++ client/layouts/404.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div
+    h2 404 error layout
+    nuxt
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+import ToolBar from "~/components/ToolBar.vue";
+
+@Component({
+  components: {
+    ToolBar
+  }
+})
+export default class extends Vue {
+  head() {
+    return {
+      title: "404",
+      htmlAttrs: {
+        lang: "ru"
+      },
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "The page did not found"
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style lang="sass">
+</style>
