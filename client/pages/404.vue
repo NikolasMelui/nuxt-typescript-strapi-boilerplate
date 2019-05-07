d5eab4eba0ca78d863899bdb70a9138ee6ee5b98
nikolasmelui <slak@samaradom.ru>
2019-05-07 21:15:45 +0400

    Add 404 page

---
+++ client/pages/404.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .404-error
+    h1 404
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+import { State, Action, Getter, Mutation, namespace } from "vuex-class";
+import { RootState, Page } from "~/types";
+
+@Component({
+  layout: "404"
+})
+export default class extends Vue {
+  head() {
+    return {
+      title: "404",
+      htmlAttrs: {
+        lang: "ru"
+      },
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "The page did not found"
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style lang="sass">
+  .404-error
+    margin: 10px
+</style>
