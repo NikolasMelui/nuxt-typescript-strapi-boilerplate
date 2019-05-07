5b370d20729453c8bf829b5c67e768c25e0b7880
nikolasmelui <slak@samaradom.ru>
2019-05-07 21:14:07 +0400

    Add BottomBar component

---
+++ client/components/BottomBar.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .bottombar
+    h4 Email - {{ email }}
+    h4 Phone - {{ phone }}
+    h4 Address - {{ address }}
+    h4 Addresslink - {{ addresslink }}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class BottomBar extends Vue {
+  @Prop(String) email!: string;
+  @Prop(String) phone!: string;
+  @Prop(String) address!: string;
+  @Prop(String) addresslink!: string;
+}
+</script>
+
+<style lang="sass">
+.bottombar
+  margin: 10px
+</style>
